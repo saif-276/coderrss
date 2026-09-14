af54de0a119d08205ad5b6fbdd9bd1c6c5f8a540
saif-276 <mohdsaifali276@gmail.com>
2026-09-14 17:37:10 +0530

    Update Arithmetic OPerator.py

--- Arithmetic OPerator.py
+++ Arithmetic OPerator.py
@@ -1,6 +1,4 @@
-#Arithmetic Operators#
-
-#1 question
+#1 
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 
@@ -10,14 +8,14 @@ print("Product =", a * b)
 print("Quotient =", a / b)
 print("Remainder =", a % b)
 
-#2 Question
+#2 
 r = float(input("Enter radius: "))
 
 area = 3.14 * r * r
 
 print("Area of the circle =", area)
 
-#3 question
+#3 
 P = float(input("Enter Principal: "))
 R = float(input("Enter Rate: "))
 T = float(input("Enter Time: "))
@@ -26,14 +24,14 @@ SI = (P * R * T) / 100
 
 print("Simple Interest =", SI)
 
-#4 question 
+#4 
 c = float(input("Enter temperature in Celsius: "))
 
 f = (c * 9 / 5) + 32
 
 print("Temperature in Fahrenheit =", f)
 
-#5 question
+#5 
 n = int(input("Enter a number: "))
 
 if n % 3 == 0 and n % 5 == 0:
