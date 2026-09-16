f56e0f220bb3edac6efb1f0266c1171cfd8d9076
saif-276 <mohdsaifali276@gmail.com>
2026-09-16 23:53:25 +0530

    Create 102.py

---
+++ 102.py
@@ -0,0 +1,8 @@
+ge(i - 1, 0, -1):
+        print(chr(64n = int(input("Enter N: "))
+for i in range(1, n + 1):
+    print(" " * (n - i), end="")
+    for j in range(1, i + 1):
+        print(chr(64 + j), end="")
+    for j in ran + j), end="")
+    print()
