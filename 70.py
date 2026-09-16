115d82cbee151baad23f3018a4dbaedf002c79d0
saif-276 <mohdsaifali276@gmail.com>
2026-09-17 01:17:39 +0530

    Create 70.py

---
+++ 70.py
@@ -0,0 +1,6 @@
+n = int(input("Enter a number: "))
+total = 0
+for i in range(1, n):
+    if n % i == 0:
+        total += i
+print(f"{n} is {'a perfect' if total == n else 'not a perfect'} number")
