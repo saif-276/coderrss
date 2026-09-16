5ca41412748cd5d8f66a2b78b9642b5529a4d9b8
saif-276 <mohdsaifali276@gmail.com>
2026-09-17 01:15:53 +0530

    Create 67.py

---
+++ 67.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a number: "))
+digits = str(n)
+power = len(digits)
+total = sum(int(d) ** power for d in digits)
+print(f"{n} is {'an Armstrong' if total == n else 'not an Armstrong'} number")
