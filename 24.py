d9aa184ef1f42aa8f51311a25980f6b91fa56715
saif-276 <mohdsaifali276@gmail.com>
2026-09-17 00:30:22 +0530

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,6 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+if username == "admin" and password == "admin123":
+    print("Login successful")
+else:
+    print("Invalid username or password")
